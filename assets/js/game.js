58f98de045aac2954cc08258b67152e15bdb82df
Your Name <natoli.missy@gmail.com>
2021-03-28 16:33:57 -0500

    added looping sequence to fight with multiple robots

--- assets/js/game.js
+++ assets/js/game.js
@@ -7,15 +7,27 @@ var playerMoney = 10;
 // You can also log multiple values at once like this
 console.log(playerName, playerAttack, playerHealth);
 
-var enemyName = "Roborto";
+var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];
+// console.log(enemyNames);
+// console.log(enemyNames[0]);
+// console.log(enemyNames[1]);
+// console.log(enemyNames[2]);
+// console.log(enemyNames.length);
+// for(var i = 0; i < enemyNames.length; i++) {
+//   console.log(enemyNames[i]);
+//   console.log(i);
+//   console.log(enemyNames[i] + " is at " + i + " index");
+// }
 var enemyHealth = 50;
 var enemyAttack = 12;
 
 // fight function
-var fight = function() {
-  // Alert players that they are starting the round
-  window.alert("Welcome to Robot Gladiators!");
-
+var fight = function(enemyName) {
+  //repeat and execute as long as the enemy-robot is alive
+  while(enemyHealth > 0) {
+    for(var i = 0; i < enemyNames.length; i++) {
+      fight(enemyNames[i]);
+  }
   // ask player if they'd like to fight or run
   var promptFight = window.prompt('Would you like to FIGHT or SKIP this battle? Enter "FIGHT" or "SKIP" to choose.');
 
@@ -68,4 +80,8 @@ var fight = function() {
 };
 
 // run fight function to start game
-fight();
+for(var i = 0; i < enemyNames.length; i++) {
+  fight(enemyNames[i]);
+}
+
+
